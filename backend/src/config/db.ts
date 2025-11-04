4944c1785de96b1ac761a3a87641a13fae8c07d6
duongbhl <duongphamhong279@gmail.com>
2025-11-04 17:19:18 +0700

    test ubuntu

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,7 @@ import Mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         await Mongoose.connect(
-            process.env.DB_URI,
+            process.env.DB_URI
         );
         console.log("MongoDB connected");
     } catch (error) {
